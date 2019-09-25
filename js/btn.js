895128169b7da066a7b4d267bf8b7444fb7f0019
KofeChen <49509963+KofeChen@users.noreply.github.com>
2019-09-25 23:08:19 +0800

    Delete btn.js

--- js/btn.js
+++
@@ -1,8 +0,0 @@
-$('.selectBtn').on('click', function() {
-  var inputObj = document.createElement('input');
-  inputObj.setAttribute('type','file');
-  inputObj.setAttribute("style",'visibility:hidden');
-  document.body.appendChild(inputObj);
-  inputObj.click();
-  inputObj.value;
-}) 
